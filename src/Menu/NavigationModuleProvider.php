e72020de1234222436f7083fb13db8a5a7f4bf0e
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2021-06-15 13:30:41 +0200

    Add default link attributes

--- src/Menu/NavigationModuleProvider.php
+++ src/Menu/NavigationModuleProvider.php
@@ -5,11 +5,11 @@ declare(strict_types=1);
 /*
  * This file is part of richardhj/contao-knp-menu.
  *
- * Copyright (c) 2020-2020 Richard Henkenjohann
+ * Copyright (c) 2020-2021 Richard Henkenjohann
  *
  * @package   richardhj/contao-knp-menu
  * @author    Richard Henkenjohann <richardhenkenjohann@googlemail.com>
- * @copyright 2020-2020 Richard Henkenjohann
+ * @copyright 2020-2021 Richard Henkenjohann
  * @license   MIT
  */
 
