8afdfe1a307e20744fb710cb425385e2141aa88c
Thomas Körner <t.koerner@heimrich-hannot.de>
2023-02-10 14:24:58 +0100

    add custom event

---
+++ src/Event/FrontendMenuEvent.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of richardhj/contao-knp-menu.
+ *
+ * (c) Richard Henkenjohann <richardhenkenjohann@googlemail.com>
+ *
+ * @license MIT
+ */
+
+namespace Richardhj\ContaoKnpMenuBundle\Event;
+
+use Contao\CoreBundle\Event\MenuEvent;
+
+class FrontendMenuEvent extends MenuEvent
+{
+}
